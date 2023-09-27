8e18a70ae46622cdfe879788bb3c1f0a308e5a59
Chicha63 <gupal2004@gmail.com>
2023-09-27 12:36:14 +0300

    Finished footer

--- src/main/ui/src/components/Footer.js
+++ src/main/ui/src/components/Footer.js
@@ -1,5 +1,22 @@
+import React from "react";
+import "./styles/Footer.css"; 
+
 const Footer = () => {
-    
-}
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-links">
+          <a href="/">Home</a>
+          <a href="https://github.com/Chicha63/ToDoList#todolist-app-project-readme">About</a>
+          <a href="mailto:gupal2004@gmail.com">Contact</a>
+        </div>
+        
+      </div>
+      <div className="footer-bottom">
+        &copy; {new Date().getFullYear()} Your Company. All Rights Reserved.
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
